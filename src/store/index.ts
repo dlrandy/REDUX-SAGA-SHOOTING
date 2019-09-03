6af55c5cb5d4ceeb2f8367020c583409853f2b2f
dlrandy <1208484996@qq.com>
2019-09-03 22:39:11 +0800

    :children_crossing: set up devtools
    set up redux devtools done

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,4 @@
-
-
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
@@ -8,8 +6,19 @@ import mySaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware)
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...[sagaMiddleware]),
+// other store enhancers if any
 );
+
+export const store = createStore(reducer, enhancer);
 sagaMiddleware.run(mySaga)
